7098dea6d820878c1688b05acdc457ea2181ae8b
Canius Chu <canius.chu@outlook.com>
2020-01-17 10:59:20 +0800

    refine UT

--- Tests/SafeDecoderTests/SafeDecoderTests.swift
+++ Tests/SafeDecoderTests/SafeDecoderTests.swift
@@ -27,18 +27,18 @@ final class SafeDecoderTests: XCTestCase {
             let array: [Int]
             let optionalArray: [Int]?
         }
-        runDecode(json: ["value": 1, "optional": 1, "array": [1,2,3], "optionalArray": [1,2,3]], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        XCTAssertSuccessReturn(try decode(json: ["value": 1, "optional": 1, "array": [1,2,3], "optionalArray": [1,2,3]], type: TestClass.self) { obj in
             XCTAssertEqual(obj.value, 1)
             XCTAssertEqual(obj.optional, 1)
             XCTAssertEqual(obj.array, [1,2,3])
             XCTAssertEqual(obj.optionalArray, [1,2,3])
-        }
-        runDecode(json: ["value": "1", "array": ["1", 2, "3"]], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        })
+        XCTAssertSuccessReturn(try decode(json: ["value": "1", "array": ["1", 2, "3"]], type: TestClass.self) { obj in
             XCTAssertEqual(obj.value, 1)
             XCTAssertNil(obj.optional)
             XCTAssertEqual(obj.array, [1,2,3])
             XCTAssertNil(obj.optionalArray)
-        }
+        })
     }
     
     func testString() {
@@ -46,10 +46,10 @@ final class SafeDecoderTests: XCTestCase {
             let value: String
             let optional: String?
         }
-        runDecode(json: ["value": "a"], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        XCTAssertSuccessReturn(try decode(json: ["value": "a"], type: TestClass.self) { obj in
             XCTAssertEqual(obj.value, "a")
             XCTAssertNil(obj.optional)
-        }
+        })
     }
     
     func testBool() {
@@ -57,52 +57,50 @@ final class SafeDecoderTests: XCTestCase {
             let value: Bool
             let optional: Bool?
         }
-        runDecode(json: ["value": true, "optional": true], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        XCTAssertSuccessReturn(try decode(json: ["value": true, "optional": true], type: TestClass.self) { obj in
             XCTAssertEqual(obj.value, true)
             XCTAssertEqual(obj.optional, true)
-        }
-        runDecode(json: ["value": false], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        })
+        XCTAssertSuccessReturn(try decode(json: ["value": false], type: TestClass.self) { obj in
             XCTAssertEqual(obj.value, false)
             XCTAssertNil(obj.optional)
-        }
-        runDecode(json: ["value": "true"], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        })
+        XCTAssertSuccessReturn(try decode(json: ["value": "true"], type: TestClass.self) { obj in
             XCTAssertEqual(obj.value, true)
-        }
-        runDecode(json: ["value": "True"], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        })
+        XCTAssertSuccessReturn(try decode(json: ["value": "True"], type: TestClass.self) { obj in
             XCTAssertEqual(obj.value, true)
-        }
-        runDecode(json: ["value": "false"], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        })
+        XCTAssertSuccessReturn(try decode(json: ["value": "false"], type: TestClass.self) { obj in
             XCTAssertEqual(obj.value, false)
-        }
-        runDecode(json: ["value": "False"], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        })
+        XCTAssertSuccessReturn(try decode(json: ["value": "False"], type: TestClass.self) { obj in
             XCTAssertEqual(obj.value, false)
-        }
+        })
     }
     
     func testEnum() {
-        
         enum TestEnum: String, Codable {
             case one
             case two
         }
-        
         struct TestClass: Codable {
             let value: TestEnum
             let optional: TestEnum?
         }
-        runDecode(json: ["value": "two", "optional": "two"], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        XCTAssertSuccessReturn(try decode(json: ["value": "two", "optional": "two"], type: TestClass.self) { obj in
             XCTAssertEqual(obj.value, TestEnum.two)
             XCTAssertEqual(obj.optional, TestEnum.two)
-        }
-        runDecode(json: ["value": "two"], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        })
+        XCTAssertSuccessReturn(try decode(json: ["value": "two"], type: TestClass.self) { obj in
             XCTAssertNil(obj.optional)
-        }
-        runDecode(json: ["value": "two", "optional": "x"], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        })
+        XCTAssertSuccessReturn(try decode(json: ["value": "two", "optional": "x"], type: TestClass.self) { obj in
             XCTAssertNil(obj.optional)
-        }
-        runDecode(json: ["value": "two", "optional": ""], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        })
+        XCTAssertSuccessReturn(try decode(json: ["value": "two", "optional": ""], type: TestClass.self) { obj in
             XCTAssertNil(obj.optional)
-        }
+        })
     }
     
     func testIntEnum() {
@@ -110,7 +108,6 @@ final class SafeDecoderTests: XCTestCase {
             case one = 1
             case two
         }
-        
         struct TestClass: Codable {
             let value: TestEnum
             let optional: TestEnum?
@@ -128,30 +125,25 @@ final class SafeDecoderTests: XCTestCase {
                 }
             }
         }
-        
-        runDecode(json: ["value": 2], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        XCTAssertSuccessReturn(try decode(json: ["value": 2], type: TestClass.self) { obj in
             XCTAssertEqual(obj.value, TestEnum.two)
             XCTAssertNil(obj.optional)
-        }
-        
-        runDecode(json: ["value": 2, "optional": 2], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        })
+        XCTAssertSuccessReturn(try decode(json: ["value": 2, "optional": 2], type: TestClass.self) { obj in
             XCTAssertEqual(obj.value, TestEnum.two)
             XCTAssertEqual(obj.optional, TestEnum.two)
-        }
-        
-        runDecode(json: ["value": "2", "optional": "2"], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        })
+        XCTAssertSuccessReturn(try decode(json: ["value": "2", "optional": "2"], type: TestClass.self) { obj in
             XCTAssertEqual(obj.value, TestEnum.two)
             XCTAssertEqual(obj.optional, TestEnum.two)
-        }
-        
-        runDecode(json: ["value": 2, "optional": "3"], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        })
+        XCTAssertSuccessReturn(try decode(json: ["value": 2, "optional": "3"], type: TestClass.self) { obj in
             XCTAssertEqual(obj.value, TestEnum.two)
             XCTAssertNil(obj.optional)
-        }
-        
-        runDecode(json: ["value": "x"], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        })
+        XCTAssertSuccessReturn(try decode(json: ["value": "x"], type: TestClass.self) { obj in
             XCTAssertEqual(obj.value, TestEnum.one)
-        }
+        })
     }
     
     func testDate() {
@@ -159,30 +151,28 @@ final class SafeDecoderTests: XCTestCase {
             let value: Date
             let optional: Date?
         }
-        runDecode(json: ["value": "2019-01-01T01:01:01Z", "optional": "2019-01-01T01:01:01Z"], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        XCTAssertSuccessReturn(try decode(json: ["value": "2019-01-01T01:01:01Z", "optional": "2019-01-01T01:01:01Z"], type: TestClass.self) { obj in
             XCTAssertNotNil(obj.optional)
-        }
-        runDecode(json: ["value": "2019-01-01T01:01:01Z", "optional": ""], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        })
+        XCTAssertSuccessReturn(try decode(json: ["value": "2019-01-01T01:01:01Z", "optional": ""], type: TestClass.self) { obj in
             XCTAssertNil(obj.optional)
-        }
+        })
     }
     
     func testArrayOfArray() {
         struct TestClass: Codable {
             let array: [[Int]]
         }
-        runDecode(json: ["array": [[1,"2",3],["1",2,"3"]]], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        XCTAssertSuccessReturn(try decode(json: ["array": [[1,"2",3],["1",2,"3"]]], type: TestClass.self) { obj in
             XCTAssertEqual(obj.array, [[1,2,3],[1,2,3]])
-        }
+        })
     }
     
     func testFallbackCodingKey() {
-        
         enum TestEnum: String, Codable {
             case one
             case two
         }
-        
         struct TestClass: Codable {
             let value: Int
             let optional: Int?
@@ -203,56 +193,59 @@ final class SafeDecoderTests: XCTestCase {
                 }
             }
         }
-
-        runDecode(json: [:], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        XCTAssertSuccessReturn(try decode(json: [:], type: TestClass.self) { obj in
             XCTAssertEqual(obj.value, 42)
             XCTAssertNil(obj.optional)
             XCTAssertEqual(obj.array, [])
             XCTAssertEqual(obj.enumValue, .one)
-        }
-        runDecode(json: ["value":123, "optional": "1"], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        })
+        XCTAssertSuccessReturn(try decode(json: ["value":123, "optional": "1"], type: TestClass.self) { obj in
             XCTAssertEqual(obj.value, 123)
             XCTAssertEqual(obj.optional, 1)
             XCTAssertEqual(obj.array, [])
-        }
-        runDecode(json: ["optional": "x", "enumValue": "x"], type: TestClass.self, fail: { error in XCTFail("\(error)") }) { obj in
+        })
+        XCTAssertSuccessReturn(try decode(json: ["optional": "x", "enumValue": "x"], type: TestClass.self) { obj in
             XCTAssertNil(obj.optional)
             XCTAssertEqual(obj.enumValue, .one)
-        }
+        })
     }
+    
+    func testComplexStructure() {
+        struct RootClass: Codable {
+            let object: TestClass
+            let array: [TestClass]
+        }
+        struct TestClass: Codable {
+            let value: Int
+        }
+        let json = ["value": "1"]
+        let validation = { (obj: TestClass) in
+            XCTAssertEqual(obj.value, 1)
+        }
+        XCTAssertSuccessReturn(try decode(json: json, type: TestClass.self) { obj in
+            validation(obj)
+        })
+        XCTAssertSuccessReturn(try decode(json: [json, json], type: [TestClass].self) { obj in
+            obj.forEach({ validation($0) })
+        })
+        XCTAssertSuccessReturn(try decode(json: ["object": json, "array": [json, json]], type: RootClass.self) { obj in
+            validation(obj.object)
+            obj.array.forEach({ validation($0) })
+        })
+    }
+    
+    private func decode<T: Codable>(json: Any, type: T.Type, validation: (_ obj: T) -> Void) throws {
+        let data = try JSONSerialization.data(withJSONObject: json, options: [])
+        let obj = try decoder.decode(T.self, from: data)
+        validation(obj)
+    }
+}
 
-    // MARK: Private
-    
-    private struct RootClass<T: Codable>: Codable {
-        let object: T
-        let array: [T]
-    }
-    
-    private func runDecode<T: Codable>(json: Any, type: T.Type, fail: (_ error: Error) -> Void, validation: (_ result: T) -> Void) {
-        do {
-            let data = try JSONSerialization.data(withJSONObject: json, options: [])
-            let result = try decoder.decode(T.self, from: data)
-            validation(result)
-        } catch {
-            fail(error)
-            return
-        }
-        do {
-            let data = try JSONSerialization.data(withJSONObject: [json, json], options: [])
-            let result = try decoder.decode([T].self, from: data)
-            result.forEach({ validation($0) })
-        } catch {
-            fail(error)
-            return
-        }
-        do {
-            let data = try JSONSerialization.data(withJSONObject: ["object": json, "array": [json, json]], options: [])
-            let result = try decoder.decode(RootClass<T>.self, from: data)
-            validation(result.object)
-            result.array.forEach({ validation($0) })
-        } catch {
-            fail(error)
-            return
-        }
+func XCTAssertSuccessReturn<T>(_ expression: @autoclosure () throws -> T, in file: StaticString = #file, line: UInt = #line) -> T {
+    do {
+        return try expression()
+    } catch {
+        XCTFail("\(error)", file: file, line: line)
+        fatalError("\(error)")
     }
 }
